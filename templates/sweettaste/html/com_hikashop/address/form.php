a42617d02929d51cf266db6228d4efefa5524be6
YEO <>
2015-09-24 17:51:27 +0300

    Работа над внешним видом личного кабинета пользователя ("Адреса")
    Work PC
    
    Joomla! 3.4.4, HikaShop Starter: 2.5.0

--- templates/sweettaste/html/com_hikashop/address/form.php
+++ templates/sweettaste/html/com_hikashop/address/form.php
@@ -6,8 +6,7 @@
  * @copyright	(C) 2010-2014 HIKARI SOFTWARE. All rights reserved.
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
-defined('_JEXEC') or die('Restricted access');
-?><h1 id="hikashop_address_form_header_iframe"><?php echo JText::_('ADDRESS_INFORMATION');?></h1>
+defined('_JEXEC') or die('Restricted access');?>
 <div id="hikashop_address_form_span_iframe">
 	<form action="<?php echo hikashop_completeLink('address&task=save'); ?>" method="post" name="hikashop_address_form" enctype="multipart/form-data">
 		<table>
--- templates/sweettaste/html/com_hikashop/address/listing.php
+++ templates/sweettaste/html/com_hikashop/address/listing.php
@@ -7,7 +7,7 @@
  * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
 defined('_JEXEC') or die('Restricted access');
-?><div id="hikashop_address_listing">
+?><div id="hikashop_address_listing" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 <?php if($this->user_id){ ?>
 <fieldset>
 	<div class="header hikashop_header_title"><h1><?php echo JText::_('ADDRESSES');?></h1></div>
@@ -36,58 +36,54 @@ defined('_JEXEC') or die('Restricted access');
 	if(!empty($this->addresses)) {
 		$ctrl = JRequest::getCmd('ctrl');
 ?>
-<div class="hikashop_address_listing_div">
-<form action="<?php echo hikashop_completeLink($ctrl); ?>" name="hikashop_user_address" method="post">
-<?php
-if(false) {
-	$this->setLayout('select');
-	echo $this->loadTemplate();
-} else {
-?>
-<table class="hikashop_address_listing_table">
-<?php
-		global $Itemid;
-		$addressClass = hikashop_get('class.address');
-		$token = hikashop_getFormToken();
-		foreach($this->addresses as $address){
-			$this->address =& $address;
-?>
-	<tr class="hikashop_address_listing_item">
-		<td class="hikashop_address_listing_item_default">
-			<input type="radio" name="address_default" value="<?php echo $this->address->address_id;?>"<?php
-				if($this->address->address_default == 1) {
-					echo ' checked="checked"';
-				}
-			?> onclick="this.form.submit();"/>
-		</td>
-		<td class="hikashop_address_listing_item_details">
-			<span><?php
-				echo $addressClass->displayAddress($this->fields, $address, 'address');
-			?></span>
-		</td>
-		<td class="hikashop_address_listing_item_actions">
-			<a href="<?php echo hikashop_completeLink('address&task=delete&address_id='.$address->address_id.'&'.$token.'=1&Itemid='.$Itemid);?>" title="<?php echo JText::_('HIKA_DELETE'); ?>"><img src="<?php echo HIKASHOP_IMAGES; ?>delete.png" alt="<?php echo JText::_('HIKA_DELETE'); ?>" /></a>
-<?php
-			echo $this->popup->display(
-				'<img src="'. HIKASHOP_IMAGES.'edit.png" title="'. JText::_('HIKA_EDIT').'" alt="'. JText::_('HIKA_EDIT').'" />',
-				'HIKA_EDIT',
-				hikashop_completeLink('address&task=edit&address_id='.$address->address_id.'&Itemid='.$Itemid, true),
-				'hikashop_edit_address_popup_'.$address->address_id,
-				760, 480, '', '', 'link'
-			);
-?>
-		</td>
-	</tr>
-<?php
-		}
-?>
-</table>
-<?php } ?>
-	<input type="hidden" name="option" value="<?php echo HIKASHOP_COMPONENT; ?>" />
-	<input type="hidden" name="ctrl" value="<?php echo $ctrl ?>" />
-	<input type="hidden" name="task" value="setdefault" />
-	<?php echo JHTML::_('form.token'); ?>
-</form>
+<div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+        <form action="<?php echo hikashop_completeLink($ctrl); ?>" name="hikashop_user_address" method="post">
+        <?php
+        if(false) {
+                $this->setLayout('select');
+                echo $this->loadTemplate();
+        } else {
+        ?>
+
+        <?php
+            global $Itemid;
+            $addressClass = hikashop_get('class.address');
+            $token = hikashop_getFormToken();
+            foreach($this->addresses as $address){
+                $this->address =& $address; ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <input type="radio" name="address_default" value="<?php echo $this->address->address_id;?>"<?php
+                        if($this->address->address_default == 1) {
+                            echo ' checked="checked"';
+                        } ?> onclick="this.form.submit();"/>
+
+                        <div class="pull-right" style="font-size: 18px">
+                            <a href="<?php echo hikashop_completeLink('address&task=delete&address_id='.$address->address_id.'&'.$token.'=1&Itemid='.$Itemid);?>" title="<?php echo JText::_('HIKA_DELETE'); ?>"><span class="glyphicon glyphicon-trash" title="<?php echo JText::_('HIKA_DELETE'); ?>"></span></a>
+                            <?php echo $this->popup->display(
+                                '<span class="glyphicon glyphicon-pencil" title="'. JText::_('HIKA_EDIT').'"></span>',
+                                'HIKA_EDIT',
+                                hikashop_completeLink('address&task=edit&address_id='.$address->address_id.'&Itemid='.$Itemid, true),
+                                'hikashop_edit_address_popup_'.$address->address_id,
+                                760, 480, '', '', 'link'
+                            ); ?>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <address><?php
+                            echo $addressClass->displayAddress($this->fields, $address, 'address'); ?>
+                        </address>
+                    </div>
+                </div> <?php
+            }
+        } ?>
+                <input type="hidden" name="option" value="<?php echo HIKASHOP_COMPONENT; ?>" />
+                <input type="hidden" name="ctrl" value="<?php echo $ctrl ?>" />
+                <input type="hidden" name="task" value="setdefault" />
+                <?php echo JHTML::_('form.token'); ?>
+        </form>
+    </div>
 </div>
 <?php
 	}
